85ddd6f5135d6ef5c0a3f67c68d6067ea7ff1020
gautam-shetty <25051997gautampshetty@gmail.com>
2025-01-08 18:31:39 -0400

    feat: add purge script and JSON/TXT merging functionality

---
+++ purge.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=code-smell-evo-purge
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+
+#SBATCH --time=15:00          # Process limit for each task
+
+#SBATCH --account=def-tusharma
+#SBATCH --mail-user=gautam@dal.ca
+#SBATCH --mail-type=ALL
+
+repo_name="code_smell_evolution_PURGE"
+
+
+echo ">>> JOB STARTED FOR $repo_name (Task ID: $SLURM_ARRAY_TASK_ID with ARG: $ARG)"
+handle_signal() 
+{
+    echo 'Trapped - Moving File'
+    rsync -axvH --no-g --no-p $SLURM_TMPDIR/$repo_name/output/* $refresearch/data/output/
+    exit 0
+}
+
+trap 'handle_signal' SIGUSR1
+
+# -------------------------------------------------------
+echo ">>> Loading modules and activating virtual environment."
+module --force purge
+module load StdEnv/2020 java/17.0.2 python/3.10
+unset JAVA_TOOL_OPTIONS
+
+virtualenv --no-download $SLURM_TMPDIR/.venv
+source $SLURM_TMPDIR/.venv/bin/activate
+pip install --no-index --upgrade pip
+pip install  --no-index -r requirements.txt
+# -------------------------------------------------------
+
+# -------------------------------------------------------
+echo -e "\n\n\n\n\n>>> Executing the purge script."
+# -u is for unbuffered output so the print statements print it to the slurm out file
+# & at the end is to run the script in background. Unless it's running in background we can't trap the signal
+python -u scripts/purge.py &
+
+PID=$!
+wait ${PID}
+
+echo -e ">>> Completed execution of the script.\n\n\n\n\n>>>Attempting to copy the output file..."
+rsync -axvH --no-g --no-p $SLURM_TMPDIR/$repo_name/output/*  $refresearch/data/output
+# -------------------------------------------------------
+
+# -------------------------------------------------------
+echo ">>> Unloading modules and deactivating virtual environment."
+deactivate
+module unload python/3.10 java/17.0.2 StdEnv/2020
+echo ">>> JOB ENDED FOR $repo_name (Task ID: $SLURM_ARRAY_TASK_ID)"
---
+++ scripts/purge.py
@@ -0,0 +1,65 @@
+import os
+import json
+import config
+
+def merge_json_files(source_path, output_file, prefix):
+    try:
+        merged_data = {}
+        files = os.listdir(source_path)
+        json_files = [
+            f for f in files 
+            if f.endswith('.json') and f.startswith(prefix) and os.path.isfile(os.path.join(source_path, f))
+        ]
+        for file in json_files:
+            file_path = os.path.join(source_path, file)
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+                merged_data.update(data)
+        with open(output_file, 'w') as f:
+            json.dump(merged_data, f, indent=2)
+        print(f"Merged data written to {output_file}")
+    except Exception as e:
+        print(f"Error while merging JSON files: {e}")
+
+def merge_txt_files(source_path, output_file, prefix):
+    try:
+        merged_rows = []
+        files = os.listdir(source_path)
+        txt_files = [
+            f for f in files 
+            if f.endswith('.txt') and f.startswith(prefix) and os.path.isfile(os.path.join(source_path, f))
+        ]
+        
+        header_added = False  # To track if the header has already been added
+
+        for file in txt_files:
+            file_path = os.path.join(source_path, file)
+            with open(file_path, 'r') as f:
+                rows = f.readlines()
+                if rows:
+                    header = rows[0].strip()  # First line as header
+                    data_rows = [row.strip() for row in rows[1:] if row.strip()]  # Remaining lines as data
+                    
+                    if not header_added:  # Add the header only once
+                        merged_rows.append(header)
+                        header_added = True
+                    
+                    merged_rows.extend(data_rows)
+
+        with open(output_file, 'w') as f:
+            f.write("\n".join(merged_rows))
+        print(f"Merged rows written to {output_file}")
+    except Exception as e:
+        print(f"Error while merging TXT files: {e}")
+
+if __name__ == "__main__":
+    # purge corpus_info
+    merge_json_files(config.CORPUS_PATH, os.path.join(config.CORPUS_PATH, "corpus_info.json"), "corpus_info_")
+        
+    # purge designite_status
+    DESIGNITE_OP_PATH = os.path.join(config.OUTPUT_PATH, "Designite_OP")
+    merge_txt_files(DESIGNITE_OP_PATH, os.path.join(DESIGNITE_OP_PATH, "designite_status.txt"), "designite_status_")
+    
+    # purge refminer_status
+    REFMINER_OP_PATH = os.path.join(config.OUTPUT_PATH, "RefMiner_OP")
+    merge_txt_files(REFMINER_OP_PATH, os.path.join(REFMINER_OP_PATH, "refminer_status.txt"), "refminer_status_")
