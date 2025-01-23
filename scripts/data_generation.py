75badd45408f959d42217b5c69988fd3d2912000
gautam-shetty <25051997gautampshetty@gmail.com>
2025-01-23 17:52:26 -0400

    fix: update corpus processing to use repository index argument and streamline output handling

--- scripts/data_generation.py
+++ scripts/data_generation.py
@@ -1,13 +1,14 @@
 import argparse
 import traceback
 import os
-import datetime
-from pathlib import Path
+import config
+import shutil
 from corpus import prepare_corpus
 from runners import Designite, RefMiner
 from utils import GitManager, ColoredStr
+from zip import zip_dir
 
-def execute_designite(username, repo_name, repo_path, branch):
+def execute_designite(idx, username, repo_name, repo_path, branch):
     """
     Collects code smells for a given repository. 
     """
@@ -20,15 +21,29 @@ def execute_designite(username, repo_name, repo_path, branch):
             success = False
         else:
             success = True
+            
     except Exception as e:
         print(ColoredStr.red(e))
         traceback.print_exc()
-    
-    current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    info_file = os.path.join(designite_runner.output_dir, f"designite_status_{current_time}.txt")
-    save_info(info_file, repo_path, branch, success)
 
-def execute_refminer(username, repo_name, repo_path, branch):
+    if success:
+        try:
+            target_dir = os.path.join(config.OUTPUT_PATH, 'Designite_OP', username, repo_name)
+            if not os.path.exists(target_dir):
+                print(f"Error: Target path '{target_dir}' does not exist.")
+                exit(1)
+            
+            zip_dir(target_dir, os.path.join(config.ZIP_LIB, f'smells_{idx}.zip'))
+            
+            # Flush the smells dataset directory after zipping
+            shutil.rmtree(target_dir)
+            print(f"Flushed smells dataset directory: {target_dir}")
+        except Exception as e:
+            print(ColoredStr.red(e))
+            traceback.print_exc()
+        
+
+def execute_refminer(idx, username, repo_name, repo_path, branch):
     """
     Collects refactorings for a given repository.
     """
@@ -40,44 +55,46 @@ def execute_refminer(username, repo_name, repo_path, branch):
     except Exception as e:
         print(ColoredStr.red(e))
         traceback.print_exc()
-    
-    current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    info_file = os.path.join(ref_miner_runner.output_dir, f"refminer_status_{current_time}.txt")
-    save_info(info_file, repo_path, branch, success)
         
-def save_info(info_file: str, repo_path: Path, branch: str, success: bool):
-    
-    file_exists = os.path.exists(info_file)
-    with open(info_file, "a") as f:
-        if not file_exists:
-            # Write header row if file doesn't exist
-            f.write("Repo Path,Branch,Success\n")
-        
-        # Append the new row of data
-        f.write(f"{repo_path},{branch},{success}\n")
+    if success:
+        try:
+            target_dir = os.path.join(config.OUTPUT_PATH, 'RefMiner_OP', username)
+            if not os.path.exists(target_dir):
+                print(f"Error: Target path '{target_dir}' does not exist.")
+                exit(1)
+            
+            zip_dir(target_dir, os.path.join(config.ZIP_LIB, f'smells_{idx}.zip')) 
+            
+            # Flush the refactoring dataset directory after zipping
+            if os.path.isdir(target_dir):
+                shutil.rmtree(target_dir)
+                print(f"Flushed refactoring dataset directory: {target_dir}")
+            else:
+                os.remove(target_dir)
+                print(f"Flushed refactoring dataset file: {target_dir}")
+        except Exception as e:
+            print(ColoredStr.red(e))
+            traceback.print_exc()
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run analysis on repo index")
     parser.add_argument("tool", type=str, help="tool to use for analysis")
     parser.add_argument("idx", type=int, help="index of the repository to process.")
     args = parser.parse_args()
-    
 
     TOOL = args.tool
     REPO_IDX = args.idx
     
     CURR_DIR = os.path.dirname(os.path.realpath(__file__))
-    repo = prepare_corpus(REPO_IDX, clone=False)
-    current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     
-    (username, repo_name, repo_path) = repo
+    (username, repo_name, repo_path) = prepare_corpus(REPO_IDX, clone=False)
     default_branch = GitManager.get_default_branch(repo_path)
     
     if default_branch:
         if TOOL == "designite":
-            execute_designite(username, repo_name, repo_path, branch=default_branch)
+            execute_designite(REPO_IDX, username, repo_name, repo_path, branch=default_branch)
         elif TOOL == "refminer":
-            execute_refminer(username, repo_name, repo_path, branch=default_branch)
+            execute_refminer(REPO_IDX, username, repo_name, repo_path, branch=default_branch)
     else:
         print(ColoredStr.red(f"Failed to get default branch for repo: {repo_path}"))
         
