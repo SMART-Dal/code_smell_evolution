ddaf3dc43f61168b6d98d9b48ba693e3045becf8
gautam-shetty <25051997gautampshetty@gmail.com>
2025-07-01 20:06:43 -0300

    refactor: streamline repo hash collection by removing unused code and enhancing file processing

--- scripts/tmp.py
+++ scripts/tmp.py
@@ -1,40 +1,34 @@
 import os
 import traceback
 from datetime import datetime
-
 import config
-from corpus import prepare_repo, flush_repo
-from utils import FileUtils, GitManager
-from data_analyzer import RepoDataAnalyzer
+from utils import FileUtils
 
 def collect_repo_hashes():
     data = {}
     data_output_path = os.path.join(config.BIN_PATH, "data", "corpus_commits.json")
     
-    repo_ids = [0, 2, 6]
+    map_stats = [
+        file_path for file_path in FileUtils.traverse_directory(config.SMELL_REF_MAP_PATH) if file_path.endswith('.stats.json') 
+    ]
     
-    for idx in repo_ids:
+    print(f"Found {len(map_stats)} map stats to process.")
+    
+    for f in map_stats:
         try:
-            (username, repo_name, repo_path) = prepare_repo(idx, clone=True)
-            branch = GitManager.get_default_branch(repo_path)
-            if not branch:
-                print(f"Failed to get default branch for repo: {repo_path}")    
-            else:
-                analyzer = RepoDataAnalyzer(username, repo_name, repo_path, branch)
-                sorted_commits: list[tuple[str, datetime]]  = analyzer.all_commits
-                
-                sorted_commits_serializable = [
-                    (commit_hash, commit_dt.isoformat() if isinstance(commit_dt, datetime) else commit_dt)
-                    for commit_hash, commit_dt in sorted_commits
-                ]
-                
-                full_repo_name = f"{repo_name}@{username}"
-                data[full_repo_name] = sorted_commits_serializable
+            map_data = FileUtils.load_json_file(f)
+            repo_full_name = os.path.basename(f).replace('.stats.json', '')
+            sorted_commits: list[tuple[str, datetime]] = map_data.get("repo_commits", [])
+            
+            sorted_commits_serializable = [
+                (commit_hash, commit_dt.isoformat() if isinstance(commit_dt, datetime) else commit_dt)
+                for commit_hash, commit_dt in sorted_commits
+            ]
+            
+            data[repo_full_name] = sorted_commits_serializable
         except Exception as e:
-            print(e)
+            print(f"Error processing {f}: {e}")
             traceback.print_exc()
-        finally:
-            flush_repo(idx)
     
     FileUtils.save_json_file(data_output_path, data)
     print(f"Data saved to {data_output_path}")
